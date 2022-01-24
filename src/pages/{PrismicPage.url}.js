e5ca5284d49b315c801da89fe987234fe74ca980
Cong Wang <findcongwang@gmail.com>
2022-01-23 22:51:45 -0500

    feat: added og_iamge to pages

--- src/pages/{PrismicPage.url}.js
+++ src/pages/{PrismicPage.url}.js
@@ -14,7 +14,7 @@ const PageTemplate = ({ data }) => {
 
   return (
     <Layout>
-      <Seo title={doc.document_display_name.text} />
+      <Seo title={doc.document_display_name.text} imageUrl={doc.og_image && doc.og_image.url} />
       <SliceZone slices={doc.body} components={components} />
     </Layout>
   )
@@ -27,6 +27,9 @@ export const query = graphql`
       data {
         document_display_name {
           text
+        }
+        og_image {
+          url
         }
         body {
           ... on PrismicSliceType {
